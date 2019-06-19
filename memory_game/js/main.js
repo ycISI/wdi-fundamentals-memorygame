9b825582317880f42a831552306f7a0a48f51e2a
ycISI <yulli.chong@infosecinnovations.com>
2019-06-19 11:59:21 -0500

    addtion after The DOM section of pre-work

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,34 +2,45 @@ console.log("Up and running!");
 
 var cards = [
 {
-	name: "queen-of-hearts",
+	name: "queen of hearts",
 	rank: "queen",
 	suit: "hearts",
-	cardImage: "images/queen-of-hearts.png"
+	cardImage: "images/images/queen-of-hearts.png"
 },
 {
-	name: "queen-of-diamonds",
+	name: "queen of diamonds",
 	rank: "queen",
 	suit: "diamonds",
-	cardImage: "images/queen-of-diamonds.png"
+	cardImage: "images/images/queen-of-diamonds.png"
 },
 {
-	name: "king-of-hearts",
+	name: "king of hearts",
 	rank: "king",
 	suit: "hearts",
-	cardImage: "images/king-of-hearts.png"
+	cardImage: "images/images/king-of-hearts.png"
 },
 {
-	name: "king-of-diamonds",
+	name: "king of diamonds",
 	rank: "king",
 	suit: "diamonds",
-	cardImage: "images/king-of-diamonds.png"
+	cardImage: "images/images/king-of-diamonds.png"
 }
 ];
 
 var cardsInPlay = [] ; 
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
 
 function checkForMatch() {
+
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 	} 
@@ -39,18 +50,18 @@ function checkForMatch() {
 	}
 }
 
-function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-	cardsInPlay.push(cards[cardId].name);
-	checkForMatch();
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	alert("User flipped " + cards[cardId].name);
 	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit); 
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+
 }
 
-flipCard(0);
-
-flipCard(2);
 
 
-
-
+createBoard();
